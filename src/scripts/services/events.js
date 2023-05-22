80f21ed76c7d55100ed89153b95df037366c1f7c
BrunoRodri <fidbruno@hotmail.com>
2023-05-22 19:05:03 -0300

    Segundo Commit

--- src/scripts/services/events.js
+++ src/scripts/services/events.js
@@ -1,9 +1,13 @@
-import { baseUrl, repositoriesQuantity } from "/src/scripts/variables.js"
+import { baseUrl, repositoriesQuantity } from '..variables.js';
 
-async function getEvents (userName) {
-  const response = await fetch(`${baseUrl}/${userName}/events`)
-  const events = await response.json()
-  return events.filter(element => element.type === 'CreateEvent' || element.type === 'pushEvent').slice(0, repositoriesQuantity)
+async function getEvents(userName) {
+  const response = await fetch(`${baseUrl}/${userName}/events`);
+  const events = await response.json();
+  return events
+    .filter(
+      element => element.type === 'CreateEvent' || element.type === 'pushEvent'
+    )
+    .slice(0, repositoriesQuantity);
 }
 
-export { getEvents }
+export { getEvents };
