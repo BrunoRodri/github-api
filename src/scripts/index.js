4b228ce0d762e8671da3acbd1e2d1518ec1e6e2a
BrunoRodri <fidbruno@hotmail.com>
2023-05-22 19:11:27 -0300

    Correção imports

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,55 +1,47 @@
-import { getUser } from "../scripts/services/user.js"
-import { getRepositories } from "./scripts/services/repositories.js"
-import { getEvents } from "./scripts/services/events.js"
+import { getUser } from './services/user.js';
+import { getRepositories } from './services/repositories.js';
+import { getEvents } from './services/events.js';
 
-import { user } from "../scripts/objects/user.js"
-import { screen } from "../scripts/objects/screen.js"
-
+import { user } from './objects/user.js';
+import { screen } from './objects/screen.js';
 
 document.getElementById('btn-search').addEventListener('click', () => {
-  const userName = document.getElementById('input-search').value
-  if(validateEmptyInput(userName)) return
-  getUserData(userName)
-})
+  const userName = document.getElementById('input-search').value;
+  if (validateEmptyInput(userName)) return;
+  getUserData(userName);
+});
 
-document.getElementById('input-search').addEventListener('keyup', (e) => {
-  const userName = e.target.value
-  const key = e.which || e.keyCode
-  const isEnterKeyPressed = key === 13
+document.getElementById('input-search').addEventListener('keyup', e => {
+  const userName = e.target.value;
+  const key = e.which || e.keyCode;
+  const isEnterKeyPressed = key === 13;
 
-  if(isEnterKeyPressed)
+  if (isEnterKeyPressed)
     // if(validateEmptyInput(userName)) return
-    getUserData(userName)
-})
+    getUserData(userName);
+});
 
-function validateEmptyInput(userName){
-  if(userName.length === 0){
-    alert('Preencha o campo com o nome do usuário do GitHub')
-    return true
+function validateEmptyInput(userName) {
+  if (userName.length === 0) {
+    alert('Preencha o campo com o nome do usuário do GitHub');
+    return true;
   }
 }
 
-async function getUserData(userName){
+async function getUserData(userName) {
+  const userResponse = await getUser(userName);
 
-    const userResponse = await getUser (userName)
+  if (userResponse.message === 'Not Found') {
+    screen.renderNotFound();
+    return;
+  }
 
-    if(userResponse.message === "Not Found"){
-      screen.renderNotFound()
-      return
-    }
+  const repositoriesResponse = await getRepositories(userName);
+  const eventsResponse = await getEvents(userName);
 
-    const repositoriesResponse = await getRepositories (userName)
-    const eventsResponse = await getEvents (userName)
+  user.setInfo(userResponse);
+  user.setRepositories(repositoriesResponse);
+  user.setEvents(eventsResponse);
 
-    user.setInfo(userResponse)
-    user.setRepositories(repositoriesResponse)
-    user.setEvents(eventsResponse)
-
-    screen.renderUser(user)
-  
-
+  screen.renderUser(user);
 }
-
-
-
-
